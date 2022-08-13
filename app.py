c4b6b9837529a5c3185006507be758f6ddaf2296
harshithaadapa <109528625+harshithaadapa@users.noreply.github.com>
2022-08-14 00:09:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib 
 
 #load the joblib model 
-model_nb = joblib.load('amazon.csv')
+model_nb = joblib.load('amazon')
 
 #user input 
 st.title("REVIEW CLASSIFIER")
@@ -12,8 +12,5 @@ ip = st.text_input("Enter your message:")
 op = model_nb.predict([ip])
 if st.button('PREDICT'):
   st.title(op[0])  #prints the output as spam or ham  
-
- 
-
 
 
